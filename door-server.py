0cf01074bd3d0adf53efb4a9a2a27d9fe7981bd8
Casey Liss <casey@caseyliss.com>
2020-05-18 22:42:33 -0400

    add garage door server first cut

---
+++ door-server.py
@@ -0,0 +1,67 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import RPi.GPIO as GPIO
+import signal
+import sys
+import time
+
+# Set up GPIO
+GPIO.setwarnings(True)
+GPIO.setmode(GPIO.BCM)
+DOOR_SENSOR_PIN = 18
+DOOR_RELAY_PIN = 4
+GPIO.setup(DOOR_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(DOOR_RELAY_PIN, GPIO.OUT)
+
+def toggle():
+    GPIO.output(DOOR_RELAY_PIN, False)
+    time.sleep(2)
+    GPIO.output(DOOR_RELAY_PIN, True)
+
+def open():
+    if GPIO.input(DOOR_SENSOR_PIN):
+        print("Door is already open")
+        return
+    toggle()
+
+def close():
+    if GPIO.input(DOOR_SENSOR_PIN) == False:
+        print("Door is already closed")
+        return
+    toggle()
+
+def noop():
+    pass
+
+def cleanUp(signal, frame):
+    print("")
+    print("Cleaning up...")
+    GPIO.cleanup()
+    sys.exit(0)
+signal.signal(signal.SIGINT, cleanUp)
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        message = "The path is {}".format(self.path)
+        action = noop
+        if self.path == "/open":
+            message = "OPEN THAT MOTHERFUCKER"
+            action = open
+        elif self.path == "/close":
+            message = "LOCK THAT SHIT UP"
+            action = close
+        elif self.path == "/state":
+            if GPIO.input(DOOR_SENSOR_PIN):
+                # Door is open
+                message = "0"
+            else:
+                # Door is closed
+                message = "1"
+    
+        self.wfile.write(bytes(message, "utf-8"))
+        action()
+        
+httpd = HTTPServer(('', 8000), SimpleHTTPRequestHandler)
+httpd.serve_forever()
